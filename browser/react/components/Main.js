e2cec69abab9932ce40bef8297db608da28ac3ab
Ken Russo <krrusso@gmail.com>
2017-07-05 18:51:00 -0400

    wednesday commit

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -1,57 +1,38 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import ReactDOM from 'react-dom';
 import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
+
 
 export default class Main extends Component {
 
   constructor (props) {
     super(props);
     this.state = {
-      albums: [],
-      selectedAlbum: {}
     };
-    this.selectAlbum = this.selectAlbum.bind(this);
-    this.deselectAlbum = this.deselectAlbum.bind(this);
-  }
 
-  componentDidMount () {
-    axios.get('/api/albums/')
-      .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
-  }
-
-  selectAlbum (albumId) {
-    axios.get(`/api/albums/${albumId}`)
-      .then(res => res.data)
-      .then(album => this.setState({
-        selectedAlbum: album
-      }));
-  }
-
-  deselectAlbum () {
-    this.setState({ selectedAlbum: {}});
   }
 
   render () {
     return (
+      <Router>
       <div id="main" className="container-fluid">
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
         <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <SingleAlbum album={this.state.selectedAlbum} /> :
-          <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
-        </div>
+          <Switch>
+              <Route exact path='/' component={AllAlbums}/>
+              <Route exact path='/albums'  component={AllAlbums}/>
+              <Route exact path='/albums/:albumId'  component={SingleAlbum}/>
+          </Switch>
+          </div>
         <Player />
       </div>
+        </Router>
     );
   }
 }
